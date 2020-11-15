51c1d58966f4a9be4b8ed4260f0302b9f09ea866
blessing333 <blessing333@naver.com>
2020-11-15 23:59:32 +0900

    롬복 추가, 의존성 우선 순위

--- src/main/java/com/blessing/core/service/OrderServiceImpl.java
+++ src/main/java/com/blessing/core/service/OrderServiceImpl.java
@@ -2,25 +2,19 @@ package com.blessing.core.service;
 
 import com.blessing.core.domain.Member;
 import com.blessing.core.domain.Order;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
-public class OrderServiceImpl implements OrderService{
-    MemberService memberService;
-    DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberService memberService, DiscountPolicy discountPolicy) {
-        this.memberService = memberService;
-        this.discountPolicy = discountPolicy;
-    }
-
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+    private final MemberService memberService;
+    private final DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, int itemPrice, String itemName) {
         Member member = memberService.findMember(memberId);
         int discountPrice = discountPolicy.discount(member, itemPrice);
-        return new Order(memberId,itemName,itemPrice,discountPrice);
+        return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 }
--- src/main/java/com/blessing/core/service/RateDiscountPolicy.java
+++ src/main/java/com/blessing/core/service/RateDiscountPolicy.java
@@ -2,9 +2,14 @@ package com.blessing.core.service;
 
 import com.blessing.core.domain.Grade;
 import com.blessing.core.domain.Member;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 
+@Component
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy{
     private final double discountRate=10;
+
     @Override
     public int discount(Member member, int price) {
         if(member.getGrade()== Grade.VIP)
